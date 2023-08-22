73620b8b55a03741bc3177e17255de855f74b486
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-08-22 20:38:42 +0300

    add ec2 to null template

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,2 +1,26 @@
+terraform {
+  required_version = "~>0.15.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "null_resource" "null" {
 }
+
+resource "aws_instance" "example" {
+     ami = data.aws_ami.ubuntu.id
+     instance_type = "t2.micro"
+     availability_zone = var.availability_zone
+
+lifecycle {
+     ignore_changes = [ami]
+     }
+ }
